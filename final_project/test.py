23a805578878d6a8c8b6406c73351ce09ee72339
banff0 <jaspereng00@gmail.com>
2023-11-24 16:48:56 -0700

    curve now working

--- final_project/test.py
+++ final_project/test.py
@@ -10,11 +10,12 @@ from math import cos, sin, radians, degrees, sqrt, acos
 from time import sleep
 
 class ArmMotor(LargeMotor):
-    def __init__(self, OUTPUT):
+    def __init__(self, OUTPUT, invert=False):
         super(ArmMotor, self).__init__(OUTPUT)
         self.position = 0
         self.STOP_ACTION_HOLD = "brake" # make it so that the motors can be moved by hand
-        
+        if invert:
+            self.POLARITY_INVERSED = "inversed"
 
     def move_angle(self, theta, spd=20, block=True):
         self.on_for_degrees(spd, theta, block = block)
@@ -40,25 +41,39 @@ x_mtr = ArmMotor(OUTPUT_C)
 # y_mtr.STOP_ACTION_HOLD = "coast"
 # x_mtr.STOP_ACTION_HOLD = "coast"
 
-dx = 0.1
-dy = 0.095
+y_mtr.move_angle(360 * .5, block=False)
+x_mtr.move_angle(360 * .25)
 
-for i in range(0, 10):
-    # y_mtr.move_angle(-360 * 0.03, spd=5 + (i/5) * 2, block=False)
-    # x_mtr.move_angle(-360 * 0.03, spd=25 - (i/5) * 2, block=True)
-    # print(5 + (i/5) * 2, 25 - (i/5) * 2)
+y_mtr.move_angle(-360 * .5, block=False)
+x_mtr.move_angle(360 * .25)
 
-    # y_mtr.move_angle(-360 * (0.5 - i/10), spd=25, block=False)
-    # x_mtr.move_angle(-360 * (0.1 + i/10), spd=25, block=True)
 
-    ratio = ((100*dx)/(100*dy))
-    x_mtr.move_angle(360 * (dx), spd = 10 * ratio, block=False)
-    y_mtr.move_angle(-360 * (dy), spd = 10, block=True)
+# ARC
+# dx = 0.2
+# dy = 0.095
 
-    dx -= 0.01
-    dy += 0.01
+# for i in range(0, 10):
+#     # y_mtr.move_angle(-360 * 0.03, spd=5 + (i/5) * 2, block=False)
+#     # x_mtr.move_angle(-360 * 0.03, spd=25 - (i/5) * 2, block=True)
+#     # print(5 + (i/5) * 2, 25 - (i/5) * 2)
 
-    print(dx, dy, 10 * ratio)
+#     # y_mtr.move_angle(-360 * (0.5 - i/10), spd=25, block=False)
+#     # x_mtr.move_angle(-360 * (0.1 + i/10), spd=25, block=True)
+#     if dy > dx:
+#         ratio = ((100*dx)/(100*dy))
+#         print(dx, dy, 10 * ratio)
+#         x_mtr.move_angle(360 * (dx), spd = 10 * ratio, block=False)
+#         y_mtr.move_angle(360 * (dy), spd = 10, block=True)
+#     else:
+#         ratio = ((100*dy)/(100*dx))
+#         print(dx, dy, 10 * ratio)
+#         x_mtr.move_angle(360 * (dx), spd = 10, block=False)
+#         y_mtr.move_angle(360 * (dy), spd = 10 * ratio, block=True)
+
+#     dx -= 0.01
+#     dy += 0.01
+
+    
     
 # y_mtr.STOP_ACTION_HOLD = "brake"
 # x_mtr.STOP_ACTION_HOLD = "brake"
@@ -83,8 +98,8 @@ for i in range(0, 10):
 
 # x_mtr.reset()
 
-
-# x_mtr.move_angle(360 * 1)
-# y_mtr.move_angle(360 * 1)
+# SQUARE
 # x_mtr.move_angle(-360 * 1)
+# y_mtr.move_angle(360 * 1)
+# x_mtr.move_angle(360 * 1)
 # y_mtr.move_angle(-360 * 1)
